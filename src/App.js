3d171f53a059da20a8a8e63c42ccde53ff8d5cb4
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2021-08-06 18:36:07 +0100

    Add barebones dashboard application with header, timeline and sidebar components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import('./pages/login'));
 const Signup = lazy(() => import('./pages/signup'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading..</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGNUP} component={Signup} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
           <Route component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
   );
 }
-
-export default App;
