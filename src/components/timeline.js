cd0d7f5e310db2779420bb87823eca2c2bd6bb1c
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2021-08-21 19:49:13 +0100

    Begin fetching suggested profiles

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,3 +1,7 @@
 export default function Timeline() {
-  return 'I am the timeline';
+  return (
+    <div className="container col-span-2">
+      <p>I am the timeline</p>
+    </div>
+  );
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,4 @@
-import { firebase } from '../lib/firebase';
+import { FieldValue, firebase } from '../lib/firebase';
 
 export async function doesUsernameExist(username) {
   const result = await firebase
@@ -17,4 +17,53 @@ export async function getUserByUserId(userId) {
     ...item.data(),
     docId: item.id
   }));
+}
+
+export async function getSuggestedProfiles(userId, following) {
+  let query = firebase.firestore().collection('users');
+
+  if (following.length > 0) {
+    query = query.where('userId', 'not-in', [...following, userId]);
+  } else {
+    query = query.where('userId', '!=', userId);
+  }
+
+  const result = await query.limit(10).get();
+
+  return result.docs.map((user) => ({
+    ...user.data(),
+    docId: user.id
+  }));
+}
+
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId,
+  profileId,
+  isFollowingProfile
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(loggedInUserDocId)
+    .update({
+      following: isFollowingProfile
+        ? FieldValue.arrayRemove(profileId)
+        : FieldValue.arrayUnion(profileId)
+    });
+}
+
+export async function updateFollowedUserFollowers(
+  profileDocId,
+  loggedInUserDocId,
+  isFollowingProfile
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(profileDocId)
+    .update({
+      followers: isFollowingProfile
+        ? FieldValue.arrayRemove(loggedInUserDocId)
+        : FieldValue.arrayUnion(loggedInUserDocId)
+    });
 }
